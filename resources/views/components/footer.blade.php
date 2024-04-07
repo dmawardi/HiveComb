d9e21ae79bd0d3e182b9da0175d140de1dae35e7
dmawardi <dmawardi@gmail.com>
2024-04-07 16:04:46 +0800

    Edited footer to have similar styling to navbar

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,8 +6,8 @@
                 &copy; {{ date('Y') }} HiveComb. All rights reserved.
             </p>
             <div class="flex space-x-6">
-                <a href="#" class="text-gray-200 hover:text-gray-400 text-sm font-medium">Privacy Policy</a>
-                <a href="#" class="text-gray-200 hover:text-gray-400 text-sm font-medium">Terms of Use</a>
+                <a href="#" class="text-gray-200 hover:text-purple-400 text-sm font-medium">Privacy Policy</a>
+                <a href="#" class="text-gray-200 hover:text-purple-400 text-sm font-medium">Terms of Use</a>
             </div>
         </div>
     </div>
