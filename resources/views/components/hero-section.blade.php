8298f8678754086348ba0a5faba3d0e4868ca1be
dmawardi <dmawardi@gmail.com>
2024-04-09 13:19:08 +0800

    Changed link in hero section to point to projects page

--- resources/views/components/hero-section.blade.php
+++ resources/views/components/hero-section.blade.php
@@ -4,13 +4,13 @@
     <!-- Content -->
     <div class="flex items-center justify-center h-full">
         <div class="text-center z-40">
-            <h1 class="text-6xl font-bold text-gray-500">
+            <h1 class="text-6xl font-bold text-gray-800">
                 Crafting Digital Experiences
             </h1>
-            <p class="text-2xl mt-4 text-bold text-gray-500">
+            <p class="text-2xl mt-4 text-bold text-gray-800">
                 Innovative web solutions tailored to bring your brand to life.
             </p>
-            <a href="#projects"
+            <a href="/projects"
                 class="mt-8 inline-block bg-yellow-400 text-gray-800 font-bold py-3 px-6 rounded-lg transform transition-all hover:scale-110">
                 See Our Work
             </a>
