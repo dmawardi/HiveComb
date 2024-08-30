81a50a7ec2633a86c47d5cf33b98e2edd341d2dd
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-08-30 16:42:50 +0800

    Removed footer links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,13 +2,13 @@
 <footer class="bg-gray-900 z-50 relative">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="py-4 flex justify-between items-center">
-            <p class="text-sm text-gray-200">
+            <p class="text-sm text-gray-200 mx-auto">
                 &copy; {{ date('Y') }} HiveComb. All rights reserved.
             </p>
-            <div class="flex space-x-6">
+            <!-- <div class="flex space-x-6">
                 <a href="#" class="text-gray-200 hover:text-purple-400 text-sm font-medium">Privacy Policy</a>
                 <a href="#" class="text-gray-200 hover:text-purple-400 text-sm font-medium">Terms of Use</a>
-            </div>
+            </div> -->
         </div>
     </div>
 </footer>
