8ad8fe40877808d5a4ab149662546347c8af9fdc
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-09-09 17:11:35 +0800

    Added Vite config to force HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        https: {
+            key: fs.readFileSync(
+                path.resolve(__dirname, "certs/localhost-key.pem")
+            ),
+            cert: fs.readFileSync(
+                path.resolve(__dirname, "certs/localhost.pem")
+            ),
+        },
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
